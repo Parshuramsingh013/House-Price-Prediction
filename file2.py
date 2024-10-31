a8a91a4e4d91de2f5b76d00cd0c9ee47096e2fa6
Parshuram Singh <parshuramsingh433@gmail.com>
2024-10-31 10:08:38 +0530

    Accuracy

--- file2.py
+++ file2.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import joblib
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
 # Load the dataset
 data = pd.read_csv("D:/Internship/Day-10-11th July/house_price_prediction_dataset.csv")
@@ -17,5 +18,15 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = LinearRegression()  
 model.fit(X_train, y_train)
 
+y_pred = model.predict(X_test)
+
+r2 = r2_score(y_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+
 # Save the trained model as a pickle file
 joblib.dump(model, 'house_price_model.pkl')
+
+print("R^2 Score:", r2)
+print("Mean Absolute Error (MAE):", mae)
+print("Mean Squared Error (MSE):", mse)
